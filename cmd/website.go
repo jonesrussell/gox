ee81595e66702542dcc059f861a3ec9727d338c8
Russell Jones <499552+jonesrussell@users.noreply.github.com>
2024-06-30 05:11:01 +0000

    Initial static html serve

--- cmd/website.go
+++ cmd/website.go
@@ -1,7 +1,8 @@
 package cmd
 
 import (
-	"fmt"
+	"log"
+	"net/http"
 
 	"github.com/spf13/cobra"
 )
@@ -17,20 +18,18 @@ Cobra is a CLI library for Go that empowers applications.
 This application is a tool to generate the needed files
 to quickly create a Cobra application.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("website called")
+		http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+			http.ServeFile(w, r, "static/index.html")
+		})
+
+		log.Println("Listening on :3000...")
+		err := http.ListenAndServe(":3000", nil)
+		if err != nil {
+			log.Fatal(err)
+		}
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(websiteCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// websiteCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// websiteCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
