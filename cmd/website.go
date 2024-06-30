147f9b858e1f24130574dc69b27aede5dab198e3
Russell Jones <499552+jonesrussell@users.noreply.github.com>
2024-06-30 09:57:27 +0000

    SRP

--- cmd/website.go
+++ cmd/website.go
@@ -1,8 +1,12 @@
 package cmd
 
 import (
+	"bytes"
+	"io"
 	"log"
 	"net/http"
+	"os"
+	"time"
 
 	"github.com/spf13/cobra"
 )
@@ -10,17 +14,13 @@ import (
 // websiteCmd represents the website command
 var websiteCmd = &cobra.Command{
 	Use:   "website",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "Create a no code website from the command line",
+	Long: `The 'website' command allows you to create a
+	no-code website directly from the command line. This is
+	particularly useful for beginner developers who need to
+	quickly	set up a static website.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-			http.ServeFile(w, r, "static/index.html")
-		})
+		http.HandleFunc("/", handleRequest)
 
 		log.Println("Listening on :3000...")
 		err := http.ListenAndServe(":3000", nil)
@@ -28,6 +28,25 @@ to quickly create a Cobra application.`,
 			log.Fatal(err)
 		}
 	},
+}
+
+func handleRequest(w http.ResponseWriter, r *http.Request) {
+	content, err := readFile("static/index.html")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	http.ServeContent(w, r, "index.html", time.Now(), bytes.NewReader(content))
+}
+
+func readFile(path string) ([]byte, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	return io.ReadAll(file)
 }
 
 func init() {
