cb755720d2881828c862a938eda79e6a65ac6c3b
Russell Jones <499552+jonesrussell@users.noreply.github.com>
2024-07-01 00:03:49 +0000

    Use menu.go

--- cmd/website.go
+++ cmd/website.go
@@ -1,11 +1,10 @@
+// cmd/website.go
 package cmd
 
 import (
-	"bufio"
 	"fmt"
 	"jonesrussell/gocreate/httpserver"
-	"os"
-	"strings"
+	"jonesrussell/gocreate/menu"
 
 	"github.com/spf13/cobra"
 )
@@ -18,59 +17,17 @@ var websiteCmd = &cobra.Command{
 	particularly useful for beginner developers who need to
 	quickly	set up a static website.`,
 	Run: func(cmd *cobra.Command, args []string) {
-		reader := bufio.NewReader(os.Stdin)
-
 		server := httpserver.NewServer()
 
-		server.Start()
-
-		for {
-			fmt.Println("\nInteractive Menu:")
-			fmt.Println("1. Change title")
-			fmt.Println("2. Exit")
-			fmt.Print("Enter command number: ")
-
-			command, _ := reader.ReadString('\n')
-			command = strings.TrimSpace(command) // Remove newline
-
-			switch command {
-			case "1":
-				err := handleChangeTitle(server)
-				if err != nil {
-					fmt.Println("Error updating title:", err)
-				}
-			case "2":
-				handleExit(server)
-				return
-			default:
-				fmt.Println("Invalid command. Please enter a number from 1 to 2.")
-			}
+		err := server.Start()
+		if err != nil {
+			fmt.Println("Error starting server:", err)
+			return
 		}
+
+		m := menu.NewMenu(server)
+		m.Display()
 	},
-}
-
-func handleChangeTitle(server *httpserver.Server) error {
-	title, err := promptForTitle()
-	if err != nil {
-		return err
-	}
-	server.UpdateTitle(title)
-	return nil
-}
-
-func handleExit(server *httpserver.Server) {
-	fmt.Println("Exiting...")
-	server.Stop()
-}
-
-func promptForTitle() (string, error) {
-	reader := bufio.NewReader(os.Stdin)
-	fmt.Print("Enter new title: ")
-	newTitle, err := reader.ReadString('\n')
-	if err != nil {
-		return "", err
-	}
-	return strings.TrimSpace(newTitle), nil // Remove newline
 }
 
 func init() {
--- menu/menu.go
+++ menu/menu.go
@@ -7,26 +7,58 @@ import (
 	"fmt"
 	"os"
 	"strings"
+
+	"jonesrussell/gocreate/httpserver"
 )
 
 type Menu struct {
 	reader *bufio.Reader
+	server *httpserver.Server
 }
 
-func NewMenu() *Menu {
+func NewMenu(server *httpserver.Server) *Menu {
 	return &Menu{
 		reader: bufio.NewReader(os.Stdin),
+		server: server,
 	}
 }
 
-func (m *Menu) Display() string {
-	fmt.Println("\nInteractive Menu:")
-	fmt.Println("1. Change title")
-	fmt.Println("2. Add tags and components")
-	fmt.Println("3. Reload server")
-	fmt.Println("4. Exit")
-	fmt.Print("Enter command number: ")
+func (m *Menu) Display() {
+	for {
+		fmt.Println("\nInteractive Menu:")
+		fmt.Println("1. Change title")
+		fmt.Println("2. Exit")
+		fmt.Print("Enter command number: ")
 
-	command, _ := m.reader.ReadString('\n')
-	return strings.TrimSpace(command) // Remove newline
+		command, _ := m.reader.ReadString('\n')
+		command = strings.TrimSpace(command) // Remove newline
+
+		switch command {
+		case "1":
+			err := m.handleChangeTitle()
+			if err != nil {
+				fmt.Println("Error updating title:", err)
+			}
+		case "2":
+			m.handleExit()
+			return
+		default:
+			fmt.Println("Invalid command. Please enter a number from 1 to 2.")
+		}
+	}
+}
+
+func (m *Menu) handleChangeTitle() error {
+	fmt.Print("Enter new title: ")
+	newTitle, err := m.reader.ReadString('\n')
+	if err != nil {
+		return err
+	}
+	m.server.UpdateTitle(strings.TrimSpace(newTitle)) // Remove newline
+	return nil
+}
+
+func (m *Menu) handleExit() {
+	fmt.Println("Exiting...")
+	m.server.Stop()
 }
